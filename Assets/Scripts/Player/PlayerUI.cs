80b9fb323b77a5942fe26ea4d5c145f8cb98f379
micle546 <micle546@gmail.com>
2022-12-15 07:16:41 -0500

    Add HealthbarUI, and testhealthpickup
    Add HealthbarUI
    Add HealthPickup

--- Assets/Scripts/Player/PlayerUI.cs
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
@@ -10,18 +11,35 @@ public class PlayerUI : MonoBehaviour
     private TextMeshProUGUI promptText;
     [SerializeField]
     private GameObject pauseMenu;
+    [SerializeField]
+    private TextMeshProUGUI healthText;
 
     private bool isPaused = false;
+    private float currentHealth = 100f;
+    public float maxHealth = 100f;
+    public float chipSpeed = 2f;
+    private float lerpTimer;
+
+    public Image frontHealthBar;
+    public Image backHealthBar;
+    
+
+    public void Start()
+    {
+        currentHealth = maxHealth;
+    }
 
     // Start is called before the first frame update
     void Awake()
     {
         Time.timeScale = 1f;
+        UpdateHealthUI();
     }
 
     public void Update()
     {
-        
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthUI();
     }
 
 
@@ -46,5 +64,44 @@ public class PlayerUI : MonoBehaviour
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
+    }
+    private void UpdateHealthUI()
+    {
+        Debug.Log(currentHealth);
+        float fillF = frontHealthBar.fillAmount;
+        float fillB = backHealthBar.fillAmount;
+        float hFraction = currentHealth / maxHealth;
+        healthText.SetText("Health: " + currentHealth + "/" + maxHealth);
+
+        if (fillB > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplete);
+        }
+        if (fillF < hFraction)
+        {
+            backHealthBar.color = Color.green;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillF, backHealthBar.fillAmount, percentComplete);
+        }
+    }
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        lerpTimer = 0f;
+        //TODO: trigger audio effect
+    }
+    public void RestoreHealth(float healAmount)
+    {
+        currentHealth += healAmount;
+        lerpTimer = 0f;
+        //TODO: trigger audio effect?
     }
 }
