6f63a38d987c54dc9f56371f5f4b52c03ac7a83f
micle546 <micle546@gmail.com>
2022-12-16 22:20:40 -0500

    LevelFinish Prefab
    Closes #42 Level finish prefab
    Fixes #44
    Fixes #43

---
+++ Assets/Scripts/Interactables/LevelFinish.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelFinish : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider loadingbar;
+    public TextMeshProUGUI loadingText;
+
+    void Start()
+    {
+        loadingScreen.SetActive(false);
+
+    }
+
+    public void NextLevel()
+    {
+        PlayerUI.instance.setPaused(true);
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        Debug.Log("Loading Scene: " + SceneManager.GetSceneByBuildIndex(nextSceneIndex).name);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        StartCoroutine(LoadAsync(nextSceneIndex));
+    }
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!op.isDone)
+        {
+            float progress = Mathf.Clamp01(op.progress / .9f);
+            loadingbar.value = progress;
+            loadingText.text = "Loading " + Mathf.Round(progress * 100) + "%";
+
+            yield return null;
+        }
+
+
+    }
+
+}
