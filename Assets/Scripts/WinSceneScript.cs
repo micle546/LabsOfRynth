043e499e3e4f19e6c309cbdebf4df14ad2ccdc66
micle546 <micle546@gmail.com>
2022-12-16 23:02:13 -0500

    Add win scene

---
+++ Assets/Scripts/WinSceneScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class WinSceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ReturnToMenu()
+    {
+        //PlayerUI.instance.setPaused(true);
+        int nextSceneIndex = 0;
+
+        Debug.Log("Loading Scene: " + SceneManager.GetSceneByBuildIndex(nextSceneIndex).name);
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
