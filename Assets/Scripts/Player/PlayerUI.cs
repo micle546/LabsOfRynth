111149a9f22bc8fef1b97ee9dda61bb59c7edc8f
micle546 <micle546@gmail.com>
2022-12-17 21:53:02 -0500

    Add loss scene

--- Assets/Scripts/Player/PlayerUI.cs
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -45,6 +45,12 @@ public class PlayerUI : MonoBehaviour
     {
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
         UpdateHealthUI();
+        if (currentHealth == 0)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            SceneManager.LoadScene(4);
+        }
     }
 
 
